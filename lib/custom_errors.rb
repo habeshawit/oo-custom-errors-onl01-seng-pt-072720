d1729a58799a9291202396af45494fcf659400dc
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-28 04:27:33 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -11,6 +11,9 @@ class Person
       begin
         raise PartnerError
       rescue PartnerError 
+      => error
+          puts error.message
+      end
     else
       person.partner = self
     end
